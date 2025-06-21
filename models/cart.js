1a5ca86711c491f7afca7f2a709a8ba8a2559b6d
ahmed <a7med.selim123456@gmail.com>
2025-06-21 15:45:13 +0300

    Add delete section

--- models/cart.js
+++ models/cart.js
@@ -4,7 +4,7 @@ const path = require("path");
 const p = path.join(path.dirname(require.main.filename), "data", "cart.json");
 
 module.exports = class Cart {
-  static addproduct(id, productPrice) {
+  static addProduct(id, productPrice) {
     fs.readFile(p, (err, fileContent) => {
       let cart = { products: [], totalPrice: 0 };
       if (!err) {
@@ -36,18 +36,34 @@ module.exports = class Cart {
       if (err) {
         return;
       }
-      const updatedCart = { ...fileContent };
+      const updatedCart = { ...JSON.parse(fileContent) };
       const product = updatedCart.products.find((prod) => prod.id === id);
-      const productQty = product.qty;
+      if(!product){
+        return;
+      }
+      let productQty = product.qty;
+      productQty =productQty-1;
       updatedCart.products = updatedCart.products.filter(
         (prod) => prod.id !== id
       );
       updatedCart.totalPrice =
         updatedCart.totalPrice - productPrice * productQty;
+        //updatedCart.
 
       fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
         console.log(err);
       });
+    });
+  }
+
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        cb(null);
+      } else {
+        cb(cart);
+      }
     });
   }
 };
