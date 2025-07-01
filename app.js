5090b77d75bc38e07dc6b90f2ea7d48bab2d727d
ahmed <a7med.selim123456@gmail.com>
2025-07-01 19:05:19 +0300

    some edit

--- app.js
+++ app.js
@@ -1,58 +1,70 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
-const errorController = require('./controllers/error');
-const User = require('./models/user');
+const errorController = require("./controllers/error");
+const User = require("./models/user");
+
+const MONGODB_URI =
+  "mongodb+srv://ahmed:ahmed-1@cluster0.jo6xr9w.mongodb.net/shop";
 
 const app = express();
-
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const authRoutes = require('./routes/auth');
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use((req, res, next) => {
-  User.findById('685fadc094eb8c3e3a5fe219')
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
 });
 
-app.use('/admin', adminRoutes);
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store, 
+    // cookie:{
+    //   httpOnly:false,
+    // }
+  })
+ 
+);
+
+//  console.log(document.cookie)
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-     "mongodb+srv://ahmed:ahmed-1@cluster0.jo6xr9w.mongodb.net/shop"
-  )
-  .then(result => {
-    User.findOne().then(user => {
+  .connect(MONGODB_URI)
+  .then((result) => {
+    User.findOne().then((user) => {
       if (!user) {
         const user = new User({
-          name: 'Max',
-          email: 'max@test.com',
+          name: "Max",
+          email: "max@test.com",
           cart: {
-            items: []
-          }
+            items: [],
+          },
         });
         user.save();
       }
     });
     app.listen(3000);
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(err);
   });
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,39 @@
-exports.getLogin = (req, res, next) => {
+const User = require('../models/user');
 
-  console.log(req.get("Cookie"));
-  res.render("../views/shop/login.ejs", {
-    path: "/login",
-    pageTitle: "login",
-    isAuthenticated: req.isLoggedIn,
+exports.getLogin = (req, res, next) => {
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader("Set-Cookie", "loggedIn=true; ");
-  res.redirect("/");
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
 };
